18a373aaa17d3e58efc1543b00f986c6daf9b2a3
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-12-07 17:17:20 -0500

    Finished 2021 files

--- 2021/input-transform.py
+++ 2021/input-transform.py
@@ -1,7 +1,7 @@
 import json
 
 # Read information out of file into list
-with open("2020/input.txt", "r") as input_file:
+with open("2021/input.txt", "r") as input_file:
     input_str: list[str] = input_file.readlines()
 
 # Strip newline characters from input
@@ -14,5 +14,5 @@ for item in input_str:
     input.append(int(item))
 
 # Put into "input.json" file for use in puzzle solving program!
-with open("2020/mrg-input.json", "w") as my_file:
+with open("2021/input.json", "w") as my_file:
     json.dump(input, my_file)
