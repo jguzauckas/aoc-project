cb2a2faa1a8d1972a588dc7fedfadfa0d00b944d
jguzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-12-07 13:46:41 -0500

    Added 2016 files

--- 2016/input-transform.py
+++ 2016/input-transform.py
@@ -8,34 +8,5 @@ with open("2016/input.txt", "r") as input_file:
 input: list[str] = input_str.split(", ")
 
 # Put into "input.json" file for use in puzzle solving program!
-with open("2016/mrg-input.json", "w") as my_file:
+with open("2016/input.json", "w") as my_file:
     json.dump(input, my_file)
-
-direction = 0
-x = 0
-y = 0
-coords = [(0, 0)]
-first = tuple()
-for inp in input:
-    if inp[0] == "L":
-        direction -= 1
-    else:
-        direction += 1
-    if direction % 4 == 0:
-        y += int(inp[1:])
-    elif direction % 4 == 1:
-        x += int(inp[1:])
-    elif direction % 4 == 2:
-        y -= int(inp[1:])
-    else:
-        x -= int(inp[1:])
-    for coord in coords:
-        if x == coord[0] and y == coord[1] and first == ():
-            first = coord
-    coords.append((x, y))
-print(x)
-print(y)
-print(abs(x) + abs(y))
-print(first)
-
-tup = tuple()
